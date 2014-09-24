c74035f81fee7489bc37e9841f13a24de9ac5e8f
phangji <chuckychoki@gmail.com>
2014-09-24 18:50:23 +0900

    no message

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ db.pool = generic_pool.Pool({
     name : 'mysql',
     create : function(callback) {
         var config = {
-            host: '172.16.100.171',
+            host: '127.0.0.1',
             port: '3306',
             user: 'isuda',
             password: 'wldus1004',
