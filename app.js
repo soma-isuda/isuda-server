c42703802747a7c86761c540ec406b14a1de964f
phangji <chuckychoki@gmail.com>
2014-10-04 23:30:31 +0900

    bodyParser 추가

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(bodyParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
