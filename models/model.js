26099f8b9d8d1f59112325084e4bb9f29378a25b
phangji <chuckychoki@gmail.com>
2014-10-04 22:07:54 +0900

    no message

--- models/model.js
+++ models/model.js
@@ -76,7 +76,7 @@ exports.insertCategoryAlarm = function (data, callback) {
     db.pool.acquire(function(err, conn) {
         if(err) console.error('err', err);
         var Query = 'insert into categoryAlarm (userId, secondId ) values ( (select id from `user` where phoneNumber = "?" ), (select id from secondCategory where name = "?" ))';
-        conn.query(Query, [ data.phoneNumber, data.secondName ], function(err, result) {
+        conn.query(Query, function(err, result) {
             console.log('insertCategoryAlarm result');
             callback(err, result);
         });
