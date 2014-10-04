15e037a24515b7ba34c446d970840b6b54e4ae46
phangji <chuckychoki@gmail.com>
2014-10-04 23:34:18 +0900

    no message

--- app.js
+++ app.js
@@ -16,9 +16,8 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('/home/isuda/serverdir/static'));  //folder for detail images shots
