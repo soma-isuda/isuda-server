88996f60deb6872c50ffbba7868edd1376d683d4
choids <dh_stz@naver.com>
2014-10-05 01:59:46 +0900

    error fix

--- models/model.js
+++ models/model.js
@@ -62,7 +62,7 @@ exports.deleteUser = function (data, callback) {
         if(err) console.error('err', err);
         var Query = 'DELETE from categoryAlarm where `userId` = (select id from `user` where `phoneNumber` = ?) ;';
         Query += ' DELETE from SMSAlarm where `userId` = (select id from `user` where `phoneNumber` = ? ) ; ';
-        Query += ' DELETE FROM user WHERE `phoneNumber` = ? ';
+        Query += ' DELETE FROM user WHERE `phoneNumber` = ? ;';
         conn.query(Query, [data, data, data], function(err, result) {
             console.log('deleteUser result');
             callback(err, result);
