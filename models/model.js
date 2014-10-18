2375362104a72ef50d97c43fcd09a2dcdd165f58
phangji <chuckychoki@gmail.com>
2014-10-19 00:59:22 +0900

    now 쿼리 수정 1,29s

--- models/model.js
+++ models/model.js
@@ -8,7 +8,7 @@ var async = require('async'),
 exports.now = function (callback) {
     db.pool.acquire(function (err, conn) {
         if (err) console.error('err', err);
-        var Query = 'select * from productInfo where productStartTime >= curdate() and productStartTime < curdate()+1 and  '
+        var Query = 'select * from productInfo where productStartTime > curdate()-1 and productStartTime < curdate()+1 and  '
             + '(      (timediff(now(), productStartTime), providerId)  '
             + ' in   (     SELECT min(timediff(  now(), productStartTime ) ), providerId  from productInfo '
             + '   where timediff(now(), productStartTime) > 0 or timediff(now(), productStartTime) = 0   '
